47429d74eca91c28f1404a69d845a8fc6146ccba
sapehdra <36782079+sapehdra@users.noreply.github.com>
2023-10-15 22:55:22 -0300

    Add files via upload

---
+++ Lista 1/Q5/script.js
@@ -0,0 +1,10 @@
+function s (n1, n2){
+    n1 + n2 
+}
+
+let n1 = parseInt(prompt("Digite o primeiro número:"))
+let n2 = parseInt(prompt("Digite o segundo número"))
+
+let res = s(n1,n2)
+alert("O resultado é: " + res)
+
